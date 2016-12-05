abfcbe5761f55eaf41a8415bcac07dc88c2db41a
kellymeryl <owner@Owners-MacBook-Air.local>
2016-12-05 17:59:46 -0500

    connected all outlets

---
+++ ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/DataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DataTableViewCell.swift
+//  ProjectTwoKellyMcNevin
+//
+//  Created by Kelly McNevin on 12/5/16.
+//  Copyright © 2016 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class DataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
