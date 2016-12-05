59ac3eab5b38f1e83be621d7b3ed1e141a4bcf6a
kellymeryl <owner@Owners-MacBook-Air.local>
2016-12-05 18:04:45 -0500

    created Article class

--- ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/DataModel.swift
+++ ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/DataModel.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+class Article {
+    
+    var title: String
+    var author: String
+    var description: String
+    var urlToImage: String
+    var urlToArticle: String
+    
+    init(title: String, author: String, description: String, urlToImage: String, urlToArticle: String){
+        self.title = title
+        self.author = author
+        self.description = description
+        self.urlToImage = urlToImage
+        self.urlToArticle = urlToArticle
+    }
+    
+}
