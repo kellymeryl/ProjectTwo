2ac3848fe1c793d13fde727ce8e834c524e27a5b
kellymeryl <owner@Owners-MacBook-Air.local>
2016-12-05 18:00:54 -0500

    Created DataModel and FindArticle swift files

---
+++ ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/FindArticle.swift
@@ -0,0 +1,9 @@
+//
+//  FindArticle.swift
+//  ProjectTwoKellyMcNevin
+//
+//  Created by Kelly McNevin on 12/5/16.
+//  Copyright © 2016 Kelly McNevin. All rights reserved.
+//
+
+import Foundation
