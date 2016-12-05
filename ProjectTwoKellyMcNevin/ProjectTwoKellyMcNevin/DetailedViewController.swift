255fa78ef06b2e3adae75987d02b45dc2d4c8f83
kellymeryl <owner@Owners-MacBook-Air.local>
2016-12-05 18:21:18 -0500

    Created second viwe controller and connected outlets

---
+++ ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/DetailedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailedViewController.swift
+//  ProjectTwoKellyMcNevin
+//
+//  Created by Kelly McNevin on 12/5/16.
+//  Copyright © 2016 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var imageDisplay: UIImageView!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
