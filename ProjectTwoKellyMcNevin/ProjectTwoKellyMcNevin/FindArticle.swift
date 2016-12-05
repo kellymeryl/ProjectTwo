3401aedbc92f000cdce9bde4a4200d2d0557cd98
kellymeryl <owner@Owners-MacBook-Air.local>
2016-12-05 18:06:43 -0500

    created getData and getArticles functions

--- ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/FindArticle.swift
+++ ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/FindArticle.swift
@@ -11,11 +11,38 @@ import UIKit
 
 class WallStreetJournalAPIClient{
     
-    func getData(completion: @escaping () -> ()) {
+    func getData(completion: @escaping ([Article]?) -> ()) {
         
+        let endpoint = ""
+        let url = URLRequest(url: URL(string: endpoint)!)
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: url) { data, _, _ in
+            let json = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String: Any]
+            let articles = self.getArticles(json)
+            
+            DispatchQueue.main.async {
+                completion(articles)
+            }
+        }
+        task.resume()
+
     }
     
-    func getArticles(_ json: [String: Any]) -> [] {
+    func getArticles(_ json: [String: Any]) -> [Article] {
+        let listOfArticles = json["articles"] as! [[String: Any]]
         
+        var articles = [Article]()
+        
+        for jsonArticle in listOfArticles {
+            let author = jsonArticle["author"] as! String
+            let title = jsonArticle["title"] as! String
+            let description = jsonArticle["description"] as! String
+            let urlToImage = jsonArticle["urlToImage"] as! String
+            let urlToArticle = jsonArticle["url"] as! String
+            let article = Article(title: title, author: author, description: description,urlToImage: urlToImage, urlToArticle: urlToArticle)
+            articles.append(article)
+        }
+        return articles
+
     }
 }
