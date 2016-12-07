c891066b247d8441704cc3933377ca5267a86573
kellymeryl <owner@Owners-MacBook-Air.local>
2016-12-07 14:24:41 -0500

    update cnn files

---
+++ ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/CNNViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CNNViewController.swift
+//  ProjectTwoKellyMcNevin
+//
+//  Created by Kelly McNevin on 12/7/16.
+//  Copyright © 2016 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class CNNViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+}
