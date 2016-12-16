6d0c35d5f603492773bc6993135bfb8db3e843cb
kellymeryl <owner@Owners-MacBook-Air.local>
2016-12-15 20:06:55 -0500

    update

---
+++ ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/CategoryDisplayViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryDisplayViewController.swift
+//  ProjectTwoKellyMcNevin
+//
+//  Created by Kelly McNevin on 12/15/16.
+//  Copyright © 2016 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class CategoryDisplayViewController: UIViewController {
+
+    @IBOutlet weak var categoryArticleTableView: UITableView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ ProjectTwoKellyMcNevin/ProjectTwoKellyMcNevin/CategoryViewTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryViewTableViewCell.swift
+//  ProjectTwoKellyMcNevin
+//
+//  Created by Kelly McNevin on 12/15/16.
+//  Copyright © 2016 Kelly McNevin. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryArticleTitle: UILabel!
+    @IBOutlet weak var categoryArticleDescription: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
